f0198aa977c82187ea676f79f246e79c1aa05f4e
FelipeCevallos <33050195+FelipeCevallos@users.noreply.github.com>
2024-10-28 18:51:33 -0500

    First commit: added OpenZeppelin contracts

---
+++ src/govToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+
